d4e60b8dad2a1a8028d5e7b26034befce065b1ca
FuruiFuyunoTaiyou <wchen10@stuy.edu>
2017-09-28 21:21:14 -0400

    completed example of strcpy

--- hw04.c
+++ hw04.c
@@ -13,15 +13,26 @@ int main(){
 	- destination - pointer to the array into which the string is to be 
 	copied
 	- source - pointer to the array from which the string is to be copied
+	    - course is a const char to ensure that the value of the variable 
+	    will not be [unintentionally] changed/written to due to this 
+	    function
         - copies the string (including the null character) pointed at by source
 	into the character array destination
 	- returns the character array (i.e. string) destination
    */
 
-  strcpy(str, "first");
-  strcpy(str1, str);
+  printf("&\"first\" = %p\n", &"first");
+  printf("str = %p\n", str);
+  printf("str1 = %p\n", str1);
 
-  printf("str: \"%s\"", str);
+  printf("strcpy(str, \"first\") returns %p\n", strcpy(str, "first"));
+  printf("strcpy(str1, str) returns %p\n", strcpy(str1, str));
+
+  printf("str: \"%s\"\n", str);
+  printf("str1: \"%s\"\n", str1);
+
+  printf("strcpy(str, \"not first\") returns %p\n", strcpy(str, "not first"));
+  printf("str: \"%s\"\n", str);
 
   /* 
      strncpy header:
